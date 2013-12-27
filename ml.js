706553cf53459f143ef0b1b939a5bf566d8a3c6a
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-12-28 00:21:10 +0100

    implemented decision tree as well as its basic learning algorithm

--- ml.js
+++ ml.js
@@ -1,22 +1,7 @@
 (function(window) {
-	var getStepFunction = function(threshold) {
-		return function stepFunction(input) {
-			return input > threshold ? 1 : -1;
-		};
-	};
-	
-	var getLinearCombinationFunction = function(weights, size) {
-		return function linearCombination(instance) {
-			var sum = weights[0];
-			for(var i = 1; i <= size; i++)
-				sum += instance[i-1] * weights[i];
-			return sum;
-		};
-	};
-	
+
 	// define namespace
 	var ml = {};
-	ml.nn = {};
 	
 	/*
 	 * trainingSet
@@ -24,120 +9,211 @@
 	ml.TrainingSet = function() {
 		this.size = 0;
 		this.instances = [];
-		this.labels = [];
 	};
-
+	
 	ml.TrainingSet.prototype.add = function(instance, label) {
 		this.size++;
-		this.instances.push(instance);
-		this.labels.push(label);
+		this.instances.push({
+			input: instance,
+			output: label
+		});
+	};
+	
+	ml.TrainingSet.prototype.getInstances = function() {
+		return this.instances;
+	};
+	
+	ml.TrainingSet.prototype.setInstances = function(instances) {
+		this.size = instances.length;
+		this.instances = instances;
+		
+		return this;
+	};
+	
+	ml.TrainingSet.prototype.getNumberOfInstances = function() {
+		return this.size;
+	};
+	
+	// Split the TrainingSet into multiple TrainingSets using the discrete values of given feature.
+	ml.TrainingSet.prototype.splitIntoSubsets = function(featureIndex) {
+		var rawSplit = _.groupBy(this.instances, function(instance) {
+			return instance.input[featureIndex];
+		});
+		
+		// wrap into TrainingSets
+		for(var i in rawSplit) {
+			rawSplit[i] = new ml.TrainingSet().setInstances(rawSplit[i]);
+		}
+		
+		return rawSplit;
 	};
 	
 	/*
+	 * ---------------------------------------------------------------------------
 	 * models
+	 * ---------------------------------------------------------------------------
 	 */
 	
-	// Simplest ANN: Perceptron
-	ml.nn.Perceptron = function(inputSize) {
-		this.inputSize = inputSize;
-
-		// default weights
-		this.weights = [];
-		this.assignRandomWeights();
+	/*
+	 * DECISION TREE on discrete attribute values.
+	 */
+	ml.DecisionTree = function() {
 		
-		// default functions
-		this.linkFunction = getLinearCombinationFunction(this.weights, this.inputSize);
-		this.activationFunction = getStepFunction(0);
 	};
 	
-	// assign randow weights (with w_0 as additional weight)
-	ml.nn.Perceptron.prototype.assignRandomWeights = function() {
-		for(var i = 0; i <= this.inputSize; i++)
-			this.weights[i] = Math.random();
+	ml.DecisionTree.InternalNode = function(indexOfFeatureToSplit) {
+		this.indexOfFeatureToSplit = indexOfFeatureToSplit;
+		this.children = {};
 	};
 	
-	ml.nn.Perceptron.prototype.classify = function(instance) {
-		return this.activationFunction(this.linkFunction(instance));
+	// Add child node with given key to the tree.
+	ml.DecisionTree.InternalNode.prototype.addChild = function(keyValue, child) {
+		this.children[keyValue] = child;
 	};
-
-	ml.nn.Perceptron.prototype.originalTraining = function(trainingSet, learningRate) {
-		this.assignRandomWeights();
+	
+	ml.DecisionTree.LeafNode = function(label) {
+		this.label = label;
+	};
+	
+	ml.TreeLearner = function() {
 		
-		var classifiedSomeInstancesWrong = true;
+	};
+	
+	ml.TreeLearner.impurityMeasurements = {};
+	
+	// Returns true if the instances in data are homogeneous enough to be labelled with a single label.
+	ml.TreeLearner.prototype.homogeneous = function(data) {
+		// Easiest test: are all instances labelled with the same label?
+		var labels = _.pluck(data.getInstances(), "output");
+		var uniqueLabels = _.uniq(labels);
+		return uniqueLabels.length == 1;
+	};
+	
+	// Returns the most appropriate label for a set of data instances.
+	ml.TreeLearner.prototype.label = function(data) {
+		// i.e. return the label of the majority of instances.
+		return _.chain(data.getInstances())
+			.countBy("output")
+			.pairs()
+			.max(_.last)
+			.head()
+			.value()
+	};
+	
+	// Calculate impurity using ....
+	ml.TreeLearner.prototype.calculateImpurity = function(subSets) {
+		var totalNumberOfInstances = 0;
+		var impurity = 0;
 		
-		while(classifiedSomeInstancesWrong) {
-			classifiedSomeInstancesWrong = false;
+		// Iterate all subSets.
+		for(var value in subSets) {
+			var subSet = subSets[value];
+			// Count the occurrence of each class.
+			var countByClass = _.countBy(subSet.getInstances(), "output");
+			// Get overall number of instances in this subSet.
+			var numberOfInstances = _.chain(countByClass)
+				.values()
+				.reduce(function(memo, num){ return memo + num; }, 0)
+				.value();
+			totalNumberOfInstances += numberOfInstances;
+			// k-class gini index
+			var giniIndex = 0;
+			for(var class_i in countByClass) {
+				var empericalProbability_i = countByClass[class_i]/numberOfInstances;
+				giniIndex += empericalProbability_i * (1 - empericalProbability_i);
+			}
+			// use square root of gini index as impurity measurement of each subset
+			var sqrtGiniIndex = Math.sqrt(giniIndex);
 			
-			// iterate all instances in the given trainingSet
-			for(var i = 0; i < trainingSet.size; i++) {
-				// for each weight
-				for(var j = 0; j < this.weights.length; j++) {
-					// compute delta_w_j
-					var delta_w_j = learningRate * (trainingSet.labels[i] - this.classify(trainingSet.instances[i])) * (j == 0 ? 1 : trainingSet.instances[i][j-1]);
-					
-					// update w_j
-					this.weights[j] += delta_w_j;
-					
-					// found an incorrectly classified instance -> Then take another iteration
-					if(delta_w_j != 0) classifiedSomeInstancesWrong = true;
-				}
+			// since we use mutually exclusive sets, we define the overall impurity as weighted average of all subset impurities.
+			impurity += numberOfInstances * sqrtGiniIndex;
+		}
+		
+		// divide by total number of instances to average the impurity.
+		impurity /= totalNumberOfInstances;
+
+		return impurity;
+	};
+	
+	// Returns the best set of literals to be put at the root of the tree.
+	ml.TreeLearner.prototype.bestSplit = function(data, features) {
+		var minIndex = -1;
+		var impurityMin = 1;
+		
+		for(var i in features) {
+			var feature = features[i];
+			var subSets = data.splitIntoSubsets(i);
+			var impurity = this.calculateImpurity(subSets);
+			if(impurity < impurityMin) {
+				impurityMin = impurity;
+				minIndex = i;
 			}
 		}
+		return minIndex;
 	};
 	
-	ml.nn.Perceptron.prototype.gradientDescent = function(trainingSet, learningRate, acceptedErrorThreshold) {
-		this.assignRandomWeights();
+	// Grow a DECISION TREE from training data.
+	ml.TreeLearner.prototype.growTree = function(data, features) {
 		
-		var squaredError = Number.MAX_VALUE;
-		
-		while(squaredError > acceptedErrorThreshold) {
-			// initialize all delta_w_i to zero
-			var delta_w = [];
-			for(var i = 0; i < this.weights.length; i++) delta_w.push(0);
-			
-			// iterate all instances in the given trainingSet
-			for(var i = 0; i < trainingSet.size; i++) {
-				// for each weight
-				for(var j = 0; j < this.weights.length; j++) {
-					// accumulate delta_w_j (learn update)
-					delta_w[j] += learningRate * (trainingSet.labels[i] - this.classify(trainingSet.instances[i])) * (j == 0 ? 1 : trainingSet.instances[i][j-1]);
-				}
-			}
-			
-			// update all w_i at the end of an iteration
-			for(var i = 0; i < delta_w.length; i++) this.weights[i] += delta_w[i];
-			
-			// compute new Error using squaredError function
-			squaredError = 0;
-			for(var i = 0; i < trainingSet.size; i++) {
-				squaredError += Math.pow((trainingSet.labels[i] - this.classify(trainingSet.instances[i])), 2);
-			}
-			squaredError /= 2;
-		}
-	};
-	
-	ml.nn.Perceptron.prototype.stochasticGradientDescent = function() {};
+		// if data is already homogeneous enough, assign an appropriate label.
+		if(this.homogeneous(data))
+			return new ml.DecisionTree.LeafNode(this.label(data));
 
-	// complex types
-	ml.nn.DoubleLayerPerceptron = function() {};
-	ml.nn.MultiLayerPerceptron = function(layerSizes) {};
+		// find best possible split
+		var s = this.bestSplit(data, features); // s = index of feature to split with
+
+		// This node is no leaf node but an internal node.
+		var node = new ml.DecisionTree.InternalNode(s);
+		
+		// split into subsets
+		var subSets = data.splitIntoSubsets(s);
+		
+		console.log(data);
+		for(var i in subSets) {
+			var subSet = subSets[i];
+			if(subSet.getNumberOfInstances() != 0)
+				node.addChild(i, this.growTree(subSet, features));
+			else
+				node.addChild(i, new ml.DecisionTree.LeafNode(this.label(data)));
+		}
+		// return node whose label is s
+		return node;
+	};
 	
 	// make globally visible
 	window.ml = ml;
 })(window);
 
+
 // sample usage
 var trainingSet = new ml.TrainingSet();
-trainingSet.add([10,0,0], 1);
-trainingSet.add([5,3,-1], 1);
-trainingSet.add([8,0,0], 1);
-trainingSet.add([6,1,0], 1);
-trainingSet.add([0,2,10], -1);
-trainingSet.add([1,2,12], -1);
-trainingSet.add([0,0,30], -1);
+trainingSet.add(["small","short","low"], 1);
+trainingSet.add(["small","short","medium"], 1);
+trainingSet.add(["small","long","low"], 1);
+trainingSet.add(["large","short","low"], 1);
+trainingSet.add(["large","short","medium"], -1);
+trainingSet.add(["large","short","high"], -1);
+trainingSet.add(["large","long","high"], -1);
 
-var p = new ml.nn.Perceptron(3);
-//p.originalTraining(trainingSet, 0.1);
-p.gradientDescent(trainingSet, 0.1, 0.05);
+// FEATURES
+var features = [
+    {
+    	name: "abdominal girth",
+		domain: ["small", "large"]
+	},
+    {
+		name: "size",
+		domain: ["short", "long"]
+	},
+    {
+		name: "price",
+		domain: ["low", "medium", "high"]
+	}
+];
 
+// Learning
+var treeLearner = new ml.TreeLearner();
+var decisionTree = treeLearner.growTree(trainingSet, features);
+
+console.log(decisionTree);
 console.log("ready");
