aceb305e417ea8aa5ad4012a2b6d161aba46c208
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-01-17 17:56:51 +0100

    added distance measurement for euclidean distance

--- src/ml.clustering.js
+++ src/ml.clustering.js
@@ -4,12 +4,27 @@
  * ---------------------------------------------------------------------------
  */
 
+var squaredEuclideanDistance = function(input1, input2) {
+	var sum = 0;
+	for(var i = 0; i < input1.length; i++)
+		sum += Math.pow(input1[i] - input2[i], 2);
+	return sum;
+};
+
+var euclideanDistance = function(input1, input2) {
+	return Math.sqrt(squaredEuclideanDistance(input1, input2));
+};
+
 /*
  * k-nearest neighbour classifier
  */
 ml.kNearestNeighbour = function(k, trainingsSet) {
 	this.k = k;
 	this.trainingsSet = trainingsSet;
+};
+
+ml.kNearestNeighbour.prototype.classify = function(input) {
+	
 };
 
 /*
