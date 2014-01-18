17d203af907d7d339746def87e9a31d788d81e8e
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-01-18 22:16:32 +0100

    added stub for boosting

---
+++ examples/boosting.js
@@ -0,0 +1,16 @@
+/*
+ * sample data
+ */
+var trainingSet = new ml.TrainingSet();
+trainingSet.add([1,5], 1);
+trainingSet.add([2,2], 1);
+trainingSet.add([5,8], 1);
+trainingSet.add([4,6], -1);
+trainingSet.add([9,3], -1);
+trainingSet.add([7,4], -1);
+trainingSet.add([3,1], -1);
+
+var basicClassifier = new ml.EnhancedBasicLinearClassifier()
+	.learn(trainingSet);
+
+console.log(basicClassifier.classify([4,6]));
