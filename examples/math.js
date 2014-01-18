d8c784e330a42817929d019731269e5e548d7799
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-01-18 09:35:18 +0100

    added matrix class

--- examples/math.js
+++ examples/math.js
@@ -5,6 +5,22 @@ var vec = new ml.math.Vector([4, 3]);
 var vec2 = new ml.math.Vector([6,-1]);
 
 console.log(vec.length());
-console.log(vec.scalarProduct(2));
+vec.scalarProduct(2).print();
 console.log(vec.dotProduct(vec2));
 
+var mat = new ml.math.Matrix([
+		[1,2,3],
+		[4,5,6]
+]);
+
+var mat2 = new ml.math.Matrix([
+		[1,1],
+		[2,3],
+		[4,5]
+]);
+
+mat.transpose().print();
+mat.scalarProduct(3.5).print();
+mat.add(mat).print();
+mat.mulMatrix(mat2).print();
+mat2.mulMatrix(mat).print();
--- src/ml.math.js
+++ src/ml.math.js
@@ -28,3 +28,72 @@ ml.math.Vector.prototype.dotProduct = function(otherVector) {
 		.value();
 };
 
+ml.math.Vector.prototype.print = function() {
+	console.log(this.values);
+};
+
+/*
+ * Matrix
+ */
+ml.math.Matrix = function(values) {
+	this.values = values;
+	this.numberOfRows = values.length;
+	this.numberOfColumns = values[0].length;
+};
+
+ml.math.Matrix.prototype.transpose = function() {
+	return new ml.math.Matrix(_.zip.apply(_, this.values));
+};
+
+ml.math.Matrix.prototype.scalarProduct = function(scalar) {
+	return new ml.math.Matrix(_.map(this.values, function(row) {
+		return _.map(row, function(number) {
+			return scalar * number;
+		});
+	}));
+};
+
+ml.math.Matrix.prototype.add = function(otherMatrix) {
+	return new ml.math.Matrix(
+		_.chain(this.values)
+			.zip(otherMatrix.values)
+			.map(function(rows) {
+				return _.chain(rows[0])
+					.zip(rows[1])
+					.map(function(elements) {
+						return elements[0] + elements[1];
+					})
+					.value();
+			})
+			.value()
+	);
+};
+
+ml.math.Matrix.prototype.mulMatrix = function(otherMatrix) {
+	
+	// zero-initialized matrix
+	var resultValues = [];
+	for(var i = 0; i < this.numberOfRows; i++) {
+		var row = [];
+		for(var j = 0; j < otherMatrix.numberOfColumns; j++) {
+			row.push(0);
+		}
+		resultValues.push(row);
+	}
+
+	for(var i = 0; i < this.numberOfRows; i++)
+    	for(var j = 0; j < otherMatrix.numberOfColumns; j++)
+        	for(var k = 0; k < this.numberOfColumns; k++)
+        		resultValues[i][j] += this.values[i][k] * otherMatrix.values[k][j];
+
+	return new ml.math.Matrix(resultValues);
+};
+
+ml.math.Matrix.prototype.print = function() {
+	console.group('Matrix');
+	_.each(this.values, function(row) {
+		console.log(row);
+	});
+	console.groupEnd();
+};
+
