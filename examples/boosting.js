17567ec68489939deb882293ded11ff905637ddb
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-01-19 18:20:22 +0100

    implemented boosting using enhanced basic linear classifier

--- examples/boosting.js
+++ examples/boosting.js
@@ -10,10 +10,39 @@ trainingSet.add([9,3], -1);
 trainingSet.add([7,4], -1);
 trainingSet.add([3,1], -1);
 
-var basicClassifier = new ml.EnhancedBasicLinearClassifier()
+var trainingSet = new ml.TrainingSet();
+trainingSet.add([1,1], 1);
+trainingSet.add([1,2], 1);
+trainingSet.add([1,3], 1);
+trainingSet.add([1,4], 1);
+trainingSet.add([1,5], 1);
+trainingSet.add([2,1], 1);
+trainingSet.add([2,2], 1);
+trainingSet.add([2,3], 1);
+trainingSet.add([2,4], 1);
+trainingSet.add([2,5], 1);
+trainingSet.add([3,1], 1);
+trainingSet.add([3,2], 1);
+trainingSet.add([3,3], -1);
+trainingSet.add([3,4], -1);
+trainingSet.add([3,5], -1);
+trainingSet.add([4,1], 1);
+trainingSet.add([4,2], 1);
+trainingSet.add([4,3], -1);
+trainingSet.add([4,4], -1);
+trainingSet.add([4,5], -1);
+trainingSet.add([5,1], 1);
+trainingSet.add([5,2], 1);
+trainingSet.add([5,3], -1);
+trainingSet.add([5,4], -1);
+trainingSet.add([5.1,5], -1);
+
+var basicClassifier = new ml.BasicLinearClassifier()
 	.learn(trainingSet);
 console.log("Basic", basicClassifier.classify([4,6]));
 
-var ensembledClassifier = ml.Boosting(trainingSet, 3, ml.EnhancedBasicLinearClassifier);
-console.log("Ensemble", ensembledClassifier.classify([4,6]));
+var ensembledClassifier = ml.Boosting(trainingSet, 130, ml.EnhancedBasicLinearClassifier);
+console.log("Ensemble", ensembledClassifier.classify([5,5]));
+
+console.log("Ensemble", ensembledClassifier);
 
--- src/ml.boosting.js
+++ src/ml.boosting.js
@@ -11,31 +11,29 @@
 ml.EnhancedBasicLinearClassifier = function() {};
 
 ml.EnhancedBasicLinearClassifier.prototype.learn = function(trainingSet, weights) {
-	var numberOfPositives = 0;
-	var numberOfNegatives = 0;
+	var totalPositiveWeight = 0;
+	var totalNegativeWeight = 0;
 	var instances = trainingSet.getInstances();
 
 	var p = new ml.math.Vector(_.range(instances[0].input.length).map(function () { return 0; }));
 	var n = new ml.math.Vector(_.range(instances[0].input.length).map(function () { return 0; }));
 	
-	_.each(instances, function(instance) {
+	// calculate p (n) as weighted average of all positive (negative) instances
+	_.each(instances, function(instance, i) {
+		var currentWeight = weights[i];
 		if(instance.output == 1) {
-			p = p.add(new ml.math.Vector(instance.input));
-			numberOfPositives++;
+			p = p.add(new ml.math.Vector(instance.input).scalarProduct(currentWeight));
+			totalPositiveWeight += currentWeight;
 		} else {
-			n = n.add(new ml.math.Vector(instance.input));
-			numberOfNegatives++;
+			n = n.add(new ml.math.Vector(instance.input).scalarProduct(currentWeight ));
+			totalNegativeWeight += currentWeight;
 		}
 	});
-	p = p.scalarProduct(1/numberOfPositives);
-	n = n.scalarProduct(1/numberOfNegatives);
+	p = p.scalarProduct(1/totalPositiveWeight);
+	n = n.scalarProduct(1/totalNegativeWeight);
 
-	p.print();
-	n.print();
-	
-	this.w = p.add(n.scalarProduct(-1)).print();
+	this.w = p.add(n.scalarProduct(-1));
 	this.t = (Math.pow(p.length(), 2) - Math.pow(n.length(), 2)) / 2;
-	console.log(this.t);
 	
 	return this;
 };
@@ -57,7 +55,11 @@ ml.EnsembleModel = function() {
 
 ml.EnsembleModel.prototype.add = function(model, confidenceFactor) {
 	this.models.push(model);
-	this.confidenceFactors.push(confidenceFactor)
+	this.confidenceFactors.push(confidenceFactor);
+};
+
+ml.EnsembleModel.prototype.adjustConfidence = function(i, confidenceFactor) {
+	this.confidenceFactors[i] = confidenceFactor;
 };
 
 ml.EnsembleModel.prototype.classify = function(x) {
@@ -73,5 +75,57 @@ ml.EnsembleModel.prototype.classify = function(x) {
  * Actual Boosting algorithm
  */
 ml.Boosting = function(trainingSet, ensembleSize, Algorithm) {
-	return new ml.EnhancedBasicLinearClassifier().learn(trainingSet);
+	var ensemble = new ml.EnsembleModel();
+	
+	// Initial weights are all 1/|trainingSet|
+	var sizeOfTrainingSet = trainingSet.getNumberOfInstances();
+	var currentWeights = _.range(sizeOfTrainingSet).map(function () {
+		return 1/sizeOfTrainingSet;
+	});
+
+	for(var i = 0; i < ensembleSize; i++) {
+		// add new model to ensemble with current weights and default confidence factor
+		var currentModel = new ml.EnhancedBasicLinearClassifier()
+			.learn(trainingSet, currentWeights);
+		var confidenceFactor = 1;
+		ensemble.add(currentModel, confidenceFactor);
+		
+		// calculate relative error of current ensemble on training set
+		var relativeError = _.reduce(trainingSet.getInstances(), function(accumulator, instance) {
+			if(ensemble.classify(instance.input) != instance.output)
+				return accumulator + 1;
+			return accumulator;
+		}, 0) / sizeOfTrainingSet;
+		console.log("relative error", relativeError);
+
+		// break if random guess would be better
+		// (relative error > 1/2)
+		if(relativeError > 1/2) {
+			// remove latest model
+			ensemble.models.length--;
+			ensemble.confidenceFactors.length--;
+			return ensemble;
+		};
+		
+		// calculate confidence factor alpha
+		// update confidence factor alpha
+		confidenceFactor = (1/2) * Math.log((1 - relativeError) / relativeError) / Math.log(Math.E);
+		ensemble.adjustConfidence(i, confidenceFactor);
+		
+		// update weights depending on if given instance was correctly classified
+		currentWeights = _.chain(currentWeights)
+			.zip(trainingSet.getInstances())
+			.map(function(pair, i) {
+				var weight = pair[0];
+				var instance = pair[1];
+				// if misclassified
+				if(ensemble.classify(instance.input) != instance.output)
+					return weight / (2 * relativeError);
+				else
+					// if correctly classified
+					return weight / (2 * (1 - relativeError));
+			})
+			.value();
+	}
+	return ensemble;
 };
